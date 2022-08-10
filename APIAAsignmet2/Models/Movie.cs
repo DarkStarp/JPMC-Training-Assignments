cb4867b40484f17da34514d9e2e4699faf53b58b
DarkStarp <dev9893186747@gmail.com>
2022-08-10 10:43:25 +0530

    React and many assignments

---
+++ APIAAsignmet2/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace APIAAsignmet2.Models
+{
+    public class Movie
+    {
+        public int movieID { get; set; }
+        public string movieName { get; set; }
+        public string Lang { get; set; }
+        public int releaseDate { get; set; }
+        public string actor { get; set; }
+        public string director { get; set; }
+    }
+}
